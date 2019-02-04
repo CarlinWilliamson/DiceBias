ce06c8b832a696bec1b398a90d3a5f9c9aa06a85
carlinwilliamson <carlin.williamson1@gmail.com>
2019-02-04 12:18:15 +1100

    less crude dice detection based off contrast

--- src/find_dice.py
+++ src/find_dice.py
@@ -21,8 +21,11 @@ class Options:
         self.verbose = options.verbose
         print(options)
 
-BOUNDING_TOLERANCE = 0
-OUTPUT_SIZE = (640,360)
+def clamp(x, lower, upper):
+    return min(max(x,lower),upper)
+
+BOUNDING_TOLERANCE = 100
+OUTPUT_SIZE = (720,480)
 
 def main():
     options = Options()
@@ -37,19 +40,62 @@ def main():
     cv2.namedWindow('grey', cv2.WINDOW_NORMAL)
     cv2.namedWindow('mask', cv2.WINDOW_NORMAL)
 
+    ret, original = cap.read()
+
     while(True):
-        ret, bgr = cap.read()
+        bgr = original.copy()
         hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
         grey = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
 
         #mask = mask_image(hsv, screen.get_mask_values())
-        cv2.multiply(grey, 1.5, grey)
+        cv2.multiply(grey, 1.75, grey)
         #grey = cv2.GaussianBlur(grey, (5,5), 0)
 
         ret, mask = cv2.threshold(grey,225,255,cv2.THRESH_BINARY_INV)
         ret, thresh = cv2.threshold(mask, 0, 255, 0)
+        contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
 
-        circles = cv2.HoughCircles(grey,cv2.HOUGH_GRADIENT,1,70, param1=50,param2=30,minRadius=60,maxRadius=90)
+        id = 0
+        for contour in contours:
+            # chuck out contours which are too small or too big
+            area = cv2.contourArea(contour)
+            print(area)
+            if area < 10000 or area > 500000:
+                continue
+
+            # calculate corners of a bounding box around the dice
+            tl = (clamp(contour[:,0,0].min() - BOUNDING_TOLERANCE, 0, bgr.shape[1]), clamp(contour[:,0,1].min() - BOUNDING_TOLERANCE, 0, bgr.shape[0]))
+            br = (clamp(contour[:,0,0].max() + BOUNDING_TOLERANCE, 0, bgr.shape[1]), clamp(contour[:,0,1].max() + BOUNDING_TOLERANCE, 0, bgr.shape[0]))
+
+            # chuck out contours whose bounding box aspect ratio is not
+            # very square
+            aspect_ratio = (br[0] - tl[0])/(br[1] - tl[1])
+            if aspect_ratio > 1.5 or aspect_ratio < 0.5:
+                continue
+            id = id + 1
+
+            #center = (math.floor((tl[0] + br[0])/2), math.floor((tl[1] + br[1])/2))
+            #color = tuple(bgr[center[1],center[0]]*1.0)
+
+            # make a copy of the bgr image then crop it to the bounding box
+            clone = bgr.copy()
+            clone = clone[tl[1]:br[1], tl[0]:br[0]]
+            cv2.imshow(str(id), clone)
+
+            # using a smaller bounding box find the average color
+            # could use this to differentiate between multiple dice
+            tight_tl = (int(clone.shape[0]*0.4), int(clone.shape[1]*0.4))
+            tight_br = (int(clone.shape[0]*0.6), int(clone.shape[1]*0.6))
+            tight = clone[tight_tl[1]:tight_br[1], tight_tl[0]:tight_br[0]]
+            color = (tight[0,0,0].max() * 1.0, tight[0,0,1].max() * 1.0, tight[0,0,2].max() * 1.0)
+
+            # draw a colored rectangle for now
+            cv2.rectangle(bgr, tl, br, color, 5)
+
+        # draw the contours in green
+        cv2.drawContours(bgr, contours, -1, (0,255,0), 2)
+
+        """circles = cv2.HoughCircles(grey,cv2.HOUGH_GRADIENT,1,70, param1=50,param2=30,minRadius=30,maxRadius=60)
 
         #print(circles)
         if circles is not None:
@@ -60,6 +106,7 @@ def main():
                 # draw the center of the circle
                 cv2.circle(bgr,(i[0],i[1]),2,(0,0,255),3)
 
+                cv2.imshow('detected circles',bgr)"""
 
         cv2.resize(bgr, OUTPUT_SIZE, bgr)
         cv2.resize(grey, OUTPUT_SIZE, grey)
@@ -67,12 +114,14 @@ def main():
         cv2.imshow("bgr", bgr)
         cv2.imshow("grey", grey)
         cv2.imshow("mask", mask)
+
     
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
+    time.sleep(9999999)
     cap.release()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
-
